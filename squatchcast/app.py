3346f93e96083cd77e1bc3e6aee5221239374e5c
Tim Renner <renner.timothy@gmail.com>
2019-05-25 10:02:57 -0500

    Added hover updating numbers to the side of the map for the squatchcast.

--- squatchcast/app.py
+++ squatchcast/app.py
@@ -10,6 +10,7 @@ from palettable.colorbrewer.sequential import YlOrRd_9 as colors
 from math import ceil
 from datetime import datetime
 from dash.dependencies import Input, Output
+from toolz import get_in
 
 load_dotenv(find_dotenv())
 
@@ -28,6 +29,8 @@ def make_layer(json_edge, color):
 def squatchcast_map(squatchcast_data):
     """ Builds the data structure required for a map of the squatchcast.
     """
+    # TODO: This resets the zoom level and center of the map.
+    # TODO: Let's not do that.
     return {
         "data": [
             {
@@ -41,6 +44,9 @@ def squatchcast_map(squatchcast_data):
                     "color": squatchcast_data.color.tolist(),
                 },
                 "hoverinfo": "text",
+                "customdata": squatchcast_data[
+                    ["temperature_high", "squatchcast", "precip_type"]
+                ].to_dict(orient="records"),
             }
         ],
         "layout": {
@@ -68,6 +74,46 @@ def squatchcast_score_distribution(squatchcast_data, date):
             "title": f"Squatchcast Scores: {date}",
             "xaxis": {"title": "Squatchcast Score"},
             "bargap": 0.05,
+        },
+    }
+
+
+def squatchcast_temp_distribution(squatchcast_data, date):
+    """ Builds the histogram for the squatchcast temperatures.
+    """
+    return {
+        "data": [
+            {
+                "type": "histogram",
+                "x": squatchcast_data.temperature_high.tolist(),
+            }
+        ],
+        "layout": {
+            "title": f"High Temperatures: {date}",
+            "xaxis": {"title": "High Temperature"},
+            "bargap": 0.05,
+        },
+    }
+
+
+def squatchcast_precip(squatchcast_data, date):
+    """ Builds the bar chart for the squatchcast precipitation types.
+    """
+    grouped_data = squatchcast_data.groupby("precip_type").agg(
+        {"precip_type": "count"}
+    )
+    return {
+        "data": [
+            {
+                "type": "bar",
+                "x": grouped_data.index.tolist(),
+                "y": grouped_data.precip_type.tolist(),
+            }
+        ],
+        "layout": {
+            "title": f"Precip Type: {date}",
+            "xaxis": {"title": "Precipitation Type"},
+            "yaxis": {"title": "Number of Locations"},
         },
     }
 
@@ -138,6 +184,40 @@ app.layout = html.Div(
             id="squatchcast-hist",
             style={"gridRow": "11/15", "gridColumn": "1/3"},
         ),
+        dcc.Graph(
+            id="temperature-hist",
+            style={"gridRow": "11/15", "gridColumn": "3/5"},
+        ),
+        dcc.Graph(
+            id="precip-bar", style={"gridRow": "11/15", "gridColumn": "5/6"}
+        ),
+        html.Div(
+            children=[
+                html.H2("", id="weather", style={"textAlign": "center"}),
+                html.H3("Weather", style={"textAlign": "center"}),
+            ],
+            style={"gridRow": "4/6", "gridColumn": "5/6"},
+        ),
+        html.Div(
+            children=[
+                html.H2(
+                    "",
+                    id="historical-sightings",
+                    style={"textAlign": "center"},
+                ),
+                html.H3("Historical Sightings", style={"textAlign": "center"}),
+            ],
+            style={"gridRow": "6/8", "gridColumn": "5/6"},
+        ),
+        html.Div(
+            children=[
+                html.H2(
+                    "", id="squatchcast-score", style={"textAlign": "center"}
+                ),
+                html.H3("Squatchcast Score", style={"textAlign": "center"}),
+            ],
+            style={"gridRow": "8/10", "gridColumn": "5/6"},
+        ),
     ],
     style={
         "display": "grid",
@@ -145,6 +225,8 @@ app.layout = html.Div(
         "gridTemplateColumns": "repeat(5, minmax(250px, 1fr))",
     },
 )
+
+# TODO: Footer, including dark sky attribution.
 
 ###############################################################################
 # CALLBACKS
@@ -167,6 +249,54 @@ def update_score_hist(day):
     return squatchcast_score_distribution(
         data.query("date==@date"), date_marks[day]
     )
+
+
+@app.callback(
+    Output("temperature-hist", "figure"), [Input("day-slider", "value")]
+)
+def update_temperature_hist(day):
+    date = dates[day]  # noqa
+    return squatchcast_temp_distribution(
+        data.query("date==@date"), date_marks[day]
+    )
+
+
+@app.callback(Output("precip-bar", "figure"), [Input("day-slider", "value")])
+def update_precip_bar(day):
+    date = dates[day]  # noqa
+    return squatchcast_precip(data.query("date==@date"), date_marks[day])
+
+
+@app.callback(
+    Output("weather", "children"), [Input("squatchcast-map", "hoverData")]
+)
+def update_high_temperature(map_hover_data):
+    if map_hover_data:
+        high_temp = get_in(
+            ["points", 0, "customdata", "temperature_high"], map_hover_data, ""
+        )
+        precipitation = get_in(
+            ["points", 0, "customdata", "precip_type"], map_hover_data, ""
+        )
+        if not precipitation:
+            precipitation = "clear"
+        return f"{int(high_temp)}\xb0F, {precipitation}"
+    else:
+        return "\n"
+
+
+@app.callback(
+    Output("squatchcast-score", "children"),
+    [Input("squatchcast-map", "hoverData")],
+)
+def update_squatchcast_score(map_hover_data):
+    if map_hover_data:
+        score = get_in(
+            ["points", 0, "customdata", "squatchcast"], map_hover_data
+        )
+        return f"{score:.3f}"
+    else:
+        return "\n"
 
 
 if __name__ == "__main__":
